762f2071d56d0b867f095d008a4018ddeb0f12c0
Dannii-web <danielajayi599@gmail.com>
2025-06-17 17:08:50 +0100

    2nd push by Daniel

---
+++ current_account.py
@@ -0,0 +1,8 @@
+from account import Account
+
+class CurrentAccount(Account):
+    def __init__(self, balance):
+        Account.__init__(self, balance)
+
+    def withdraw(self, amount):
+        super().withdraw(amount)
