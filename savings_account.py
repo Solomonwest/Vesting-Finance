a1b4f3b4c8bd30db2205ae07ce6eba97dbce52fc
Ochai Mary <hadassahochai@gmail.com>
2025-06-17 20:26:07 +0100

    Pushed by Mary

---
+++ savings_account.py
@@ -0,0 +1,18 @@
+from account import Account
+
+class SavingsAccount(Account):
+    def __init__(self, balance):
+        Account.__init__(self, balance)
+
+    def withdraw(self, amount):
+        if amount <= 10000:
+            super().withdraw(amount)
+
+        elif amount > self.balance:
+            print("Insufficient Balance")
+
+        else:
+            print("Amount exceeds limit")
+
+    def deposit(self, amount):
+        super().deposit(amount)
